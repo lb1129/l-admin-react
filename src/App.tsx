428b157958ba188b0886b4872a0fadbc0e917fa9
leiboT <superviho1@gmail.com>
2023-06-01 03:14:22 +0800

    ci: 引用antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Button } from 'antd'
+import 'antd/dist/reset.css'
 
 function App() {
   return (
     <div className="app">
+      <Button type="primary">Button</Button>
       <header className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
         <p>
