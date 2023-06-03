1253158003a45a4356ec7fb146240ac51d104d77
leiboT <superviho1@gmail.com>
2023-06-03 20:41:34 +0800

    chore: 删除无用文件及引用

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { Button } from 'antd'
-import 'antd/dist/reset.css'
 
 function App() {
-  return (
-    <div className="app">
-      <Button type="primary">Button</Button>
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
