9dc8bd1fb0fe4e73c78abd38c14af7881f8fb2db
leiboT <superviho1@gmail.com>
2023-06-03 21:27:18 +0800

    chore: antd全局配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ConfigProvider, App as AntdApp, type ThemeConfig } from 'antd'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 
 const App = () => {
-  return <RouterProvider router={router} />
+  // TODO setTheme 注入到子组件
+  const [
+    theme
+    // setTheme
+  ] = useState<ThemeConfig>({})
+  return (
+    <ConfigProvider theme={theme}>
+      <AntdApp
+        style={{
+          height: '100%'
+        }}
+      >
+        <RouterProvider router={router} />
+      </AntdApp>
+    </ConfigProvider>
+  )
 }
 
 export default App
