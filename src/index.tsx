c2cb00b9effe0f53c3f5d586469492e8fb81948c
leiboT <superviho1@gmail.com>
2023-06-04 16:13:37 +0800

    chore: 使用路径别名

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import './i18n'
-import './assets/style/global.less'
+import App from '@/App'
+import reportWebVitals from '@/reportWebVitals'
+import '@/i18n'
+import '@/assets/style/global.less'
 import 'antd/dist/reset.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
