51589e3d6aa816cc45cb959570958843c979cfb7
leiboT <superviho1@gmail.com>
2023-06-04 16:14:55 +0800

    chore: 路径别名,input.autoComplete配置，I18nextProvider配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import { ConfigProvider, App as AntdApp, type ThemeConfig } from 'antd'
+import { I18nextProvider } from 'react-i18next'
+import i18n from '@/i18n'
 import { RouterProvider } from 'react-router-dom'
-import router from './router'
+import router from '@/router'
 
 const App = () => {
   // TODO setTheme 注入到子组件
@@ -10,13 +12,15 @@ const App = () => {
     // setTheme
   ] = useState<ThemeConfig>({})
   return (
-    <ConfigProvider theme={theme}>
+    <ConfigProvider theme={theme} input={{ autoComplete: 'off' }}>
       <AntdApp
         style={{
           height: '100%'
         }}
       >
-        <RouterProvider router={router} />
+        <I18nextProvider i18n={i18n}>
+          <RouterProvider router={router} />
+        </I18nextProvider>
       </AntdApp>
     </ConfigProvider>
   )
