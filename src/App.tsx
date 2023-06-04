6d68bc37f3cb3147571472613afd4d1553716c4e
leiboT <superviho1@gmail.com>
2023-06-04 16:58:26 +0800

    chore: i18n层级调整&ant国际化配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ConfigProvider, App as AntdApp, type ThemeConfig } from 'antd'
-import { I18nextProvider } from 'react-i18next'
-import i18n from '@/i18n'
+import { useTranslation } from 'react-i18next'
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router'
+import type { Locale } from 'antd/es/locale'
+import enUS from 'antd/locale/en_US'
+import zhCN from 'antd/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
 
 const App = () => {
   // TODO setTheme 注入到子组件
@@ -11,16 +15,31 @@ const App = () => {
     theme
     // setTheme
   ] = useState<ThemeConfig>({})
+
+  const [locale, setLocal] = useState<Locale>()
+  const { i18n } = useTranslation()
+
+  useEffect(() => {
+    switch (i18n.language) {
+      case 'zh-CN':
+        dayjs.locale('zh-cn')
+        setLocal(zhCN)
+        break
+      case 'en':
+        dayjs.locale('en')
+        setLocal(enUS)
+        break
+    }
+  }, [i18n.language])
+
   return (
-    <ConfigProvider theme={theme} input={{ autoComplete: 'off' }}>
+    <ConfigProvider locale={locale} theme={theme} input={{ autoComplete: 'off' }}>
       <AntdApp
         style={{
           height: '100%'
         }}
       >
-        <I18nextProvider i18n={i18n}>
-          <RouterProvider router={router} />
-        </I18nextProvider>
+        <RouterProvider router={router} />
       </AntdApp>
     </ConfigProvider>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App'
 import reportWebVitals from '@/reportWebVitals'
-import '@/i18n'
+import { I18nextProvider } from 'react-i18next'
+import i18n from '@/i18n'
 import '@/assets/style/global.less'
 import 'antd/dist/reset.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 )
 
