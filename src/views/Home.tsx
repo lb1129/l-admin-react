f23df28e3a8322717a4e1710b2b2c34328b6132e
leiboT <superviho1@gmail.com>
2023-06-05 23:16:04 +0800

    chore: 使用PositionMap组件

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import PositionMap from '@/components/PositionMap'
 
 function Home() {
-  return <div>Home</div>
+  return (
+    <div
+      style={{
+        height: '100%'
+      }}
+    >
+      <PositionMap onChange={(record) => {}} />
+    </div>
+  )
 }
 
 export default Home
