3fde09cb71ad7dda7e234f88febbb9546130d385
leiboT <superviho1@gmail.com>
2023-06-08 01:06:06 +0800

    chore: 操作权限hook

---
+++ src/hooks/auth.ts
@@ -0,0 +1,32 @@
+import { useLocation } from 'react-router-dom'
+import { type OperateAuthType, type MenuDataItemType } from '@/store/menuDataSlice'
+import { useAppSelector } from '@/store/hook'
+
+export const operateAuthValueToDisabled = (operateAuthValue: number | undefined) => {
+  // 值为0 无权限 返回true
+  // 值为空 或 1 有权限 则返回false
+  if (operateAuthValue === 0) return true
+  return false
+}
+
+export const useAuth = () => {
+  const { pathname } = useLocation()
+  const menuData = useAppSelector((state) => state.menuData.data)
+  // 操作权限
+  let operateAuth: OperateAuthType = {}
+  const findOperateAuth = (list: MenuDataItemType[], parentPath?: string) => {
+    for (let i = 0; i < list.length; i++) {
+      const record = list[i]
+      if (`${parentPath ?? ''}${record.path}` === pathname) {
+        if (record.operateAuth) operateAuth = record.operateAuth
+        break
+      } else if (record.children && record.children.length)
+        findOperateAuth(record.children, `${parentPath ?? ''}${record.path}`)
+    }
+  }
+  findOperateAuth(menuData)
+  // TODO 其他权限...
+  return {
+    operateAuth
+  }
+}
