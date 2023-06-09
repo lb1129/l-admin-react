2f97d2dcc3ab740147f36b68950d29d3673c08b2
leiboT <superviho1@gmail.com>
2023-06-09 20:39:11 +0800

    ci: 环境变量注解

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,20 @@ import { I18nextProvider } from 'react-i18next'
 import i18n from '@/i18n'
 import { Provider } from 'react-redux'
 import store from '@/store'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import 'antd/dist/reset.css'
 import '@/assets/style/global.style'
 // TODO 文档中 增加登录流程 页面刷新流程 页面操作权限控制 等说明
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+// 根据运行或部署环境是否支持history路由模式 选取不同路由
+const Router = process.env.REACT_APP_NOT_SUPPORT_HISTORY ? HashRouter : BrowserRouter
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <I18nextProvider i18n={i18n}>
-        <BrowserRouter>
+        <Router basename={process.env.PUBLIC_URL}>
           <App />
-        </BrowserRouter>
+        </Router>
       </I18nextProvider>
     </Provider>
   </React.StrictMode>
