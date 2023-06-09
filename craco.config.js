30b396d80e6a62861f3f70eafeb7337a6dc8d8af
leiboT <superviho1@gmail.com>
2023-06-09 20:50:14 +0800

    ci: 去掉样式相关的编译配置及lint配置，样式编写使用css-in-js

--- craco.config.js
+++ craco.config.js
@@ -1,20 +1,9 @@
 const path = require('path')
-const CracoLessPlugin = require('craco-less')
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
-  },
-  plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {}
-        }
-      }
-    }
-  ]
+  }
 }
