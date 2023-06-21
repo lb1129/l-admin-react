78dc866f483ee58abfc343114943fc171b664829
leiboT <superviho1@gmail.com>
2023-06-21 23:48:42 +0800

    chore: link主题色设置

--- src/store/themeSlice.ts
+++ src/store/themeSlice.ts
@@ -1,6 +1,7 @@
 import { createAction, createReducer } from '@reduxjs/toolkit'
 import { type ThemeConfig } from 'antd'
 import { colorPrimaryLocalforage } from '@/storage/localforage'
+import { generate } from '@ant-design/colors'
 
 export const setColorPrimary = createAction<string>('setColorPrimary')
 
@@ -14,6 +15,11 @@ const initialState: ThemeConfig = {
 const reducer = createReducer(initialState, (builder) => {
   builder.addCase(setColorPrimary, (state, action) => {
     state.token!.colorPrimary = action.payload
+    // // 由一个基本色算出一个梯度色板
+    const colors = generate(action.payload)
+    state.token!.colorLink = action.payload
+    state.token!.colorLinkActive = colors[4]
+    state.token!.colorLinkHover = colors[4]
     colorPrimaryLocalforage.set(action.payload)
   })
 })
