0dbeb6e6cfe8f1193bcba221e21771ba5fb5f3e5
leiboT <superviho1@gmail.com>
2023-06-24 21:56:30 +0800

    chore: 添加动画样式

--- src/global.style.ts
+++ src/global.style.ts
@@ -4,7 +4,7 @@ injectGlobal({
   'html,body,#root': {
     height: '100%'
   },
-  '.transition-fade': {
+  '.fade': {
     '&-enter': {
       opacity: 0,
       '&-active': {
@@ -17,6 +17,38 @@ injectGlobal({
       '&-active': {
         opacity: 0,
         transition: 'opacity 300ms'
+      }
+    }
+  },
+  '.slide-left': {
+    '&-enter': {
+      transform: 'translate3d(100%, 0, 0)',
+      '&-active': {
+        transform: 'translate3d(0, 0, 0)',
+        transition: 'transform 300ms'
+      }
+    },
+    '&-exit': {
+      transform: 'translate3d(0, 0, 0)',
+      '&-active': {
+        transform: 'translate3d(-100%, 0, 0)',
+        transition: 'transform 300ms'
+      }
+    }
+  },
+  '.slide-right': {
+    '&-enter': {
+      transform: 'translate3d(-100%, 0, 0)',
+      '&-active': {
+        transform: 'translate3d(0, 0, 0)',
+        transition: 'transform 300ms'
+      }
+    },
+    '&-exit': {
+      transform: 'translate3d(0, 0, 0)',
+      '&-active': {
+        transform: 'translate3d(100%, 0, 0)',
+        transition: 'transform 300ms'
       }
     }
   }
