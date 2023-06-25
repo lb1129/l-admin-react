b72029a5d497ca20098394fca51ad46adf2af95a
leiboT <superviho1@gmail.com>
2023-06-25 16:57:21 +0800

    refactor: 重构并增强组件功能

--- src/components/LinkPlus/index.tsx
+++ src/components/LinkPlus/index.tsx
@@ -1,33 +1,83 @@
-import React, { type ReactNode, type MouseEventHandler, useMemo, forwardRef } from 'react'
-import { Link, type LinkProps } from 'react-router-dom'
+import React, {
+  type ReactNode,
+  type HTMLAttributeAnchorTarget,
+  type CSSProperties,
+  useMemo,
+  forwardRef,
+  useCallback
+} from 'react'
+import { useNavigate, type To, type Path } from 'react-router-dom'
+import classNames from 'classnames'
+import { type NavigateByIdOptions, getFullPath } from '@/router'
 import useStyles from './style'
 
-type Props = {
+interface LinkPlusProps {
   disabled?: boolean
   // onClick 优先级 高于 to
-  onClick?: MouseEventHandler<HTMLAnchorElement>
-  to?: LinkProps['to']
+  onClick?: React.MouseEventHandler<HTMLAnchorElement>
+  to?: To | number | NavigateByIdOptions
   children?: ReactNode | ReactNode[]
+  target?: HTMLAttributeAnchorTarget
+  style?: CSSProperties
+  replace?: boolean
 }
 
-const LinkPlus = forwardRef<HTMLAnchorElement, Props>((props, ref) => {
-  const styles = useStyles()
+const LinkPlus = forwardRef<HTMLAnchorElement, LinkPlusProps>(
+  ({ disabled, onClick, to, children, target, style, replace }, ref) => {
+    const styles = useStyles()
+    const navigate = useNavigate()
 
-  const className = useMemo(() => {
-    if (props.disabled) return styles.disabled
-  }, [props.disabled, styles.disabled])
+    const className = useMemo(() => {
+      return classNames(styles.wrap, {
+        [styles.disabled]: disabled
+      })
+    }, [disabled, styles])
 
-  const to = useMemo(() => {
-    if (props.disabled) return ''
-    if (props.onClick) return ''
-    return props.to ?? ''
-  }, [props.disabled, props.to, props.onClick])
+    const href = useMemo(() => {
+      if (!to) return undefined
+      if (typeof to === 'string') return to
+      if ((to as NavigateByIdOptions).id) return getFullPath(to as NavigateByIdOptions)
+      const pathObject = to as Partial<Path>
+      return `${pathObject.pathname}${pathObject.search}${pathObject.hash}`
+    }, [to])
 
-  return (
-    <Link ref={ref} to={to} onClick={props.onClick} className={className}>
-      {props.children}
-    </Link>
-  )
-})
+    const clickHandler = useCallback(
+      (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        if (disabled) {
+          e.preventDefault()
+          return
+        }
+        if (onClick) {
+          e.preventDefault()
+          onClick(e)
+          return
+        }
+        if (target !== '_blank') {
+          e.preventDefault()
+          if (typeof to === 'number') navigate(to)
+          else if (to && (to as NavigateByIdOptions).id) {
+            navigate({ ...(to as NavigateByIdOptions), replace })
+          } else {
+            navigate(to as To, { replace })
+          }
+        }
+      },
+      [navigate, onClick, to, target, disabled, replace]
+    )
+
+    return (
+      <a
+        href={href}
+        target={target}
+        ref={ref}
+        style={style}
+        onClick={clickHandler}
+        className={className}
+      >
+        {children}
+      </a>
+    )
+  }
+)
 
 export default LinkPlus
--- src/components/LinkPlus/style/index.ts
+++ src/components/LinkPlus/style/index.ts
@@ -2,6 +2,13 @@ import { useEmotionCss } from '@/utils/useEmotionCss'
 
 export default function useStyles() {
   return {
+    wrap: useEmotionCss(({ token }) => ({
+      color: token.colorLink,
+      cursor: 'pointer',
+      '&:hover': {
+        color: token.colorLinkHover
+      }
+    })),
     disabled: useEmotionCss(() => ({
       cursor: 'not-allowed',
       color: 'rgba(0, 0, 0, 0.25) !important'
