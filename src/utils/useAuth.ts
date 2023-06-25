5580e671bcb45e773a4636c676f8a483f48e5655
leiboT <superviho1@gmail.com>
2023-06-25 16:51:05 +0800

    refactor: 根据路由id获取权限

--- src/utils/useAuth.ts
+++ src/utils/useAuth.ts
@@ -1,8 +1,8 @@
-import { useLocation } from 'react-router-dom'
+import { useMatches } from 'react-router-dom'
 import type { OperateAuthType, MenuDataItemType } from '@/views/personal-center/types'
 import { useAppSelector } from '@/store/hooks'
 
-export const operateAuthValueToDisabled = (operateAuthValue: number | undefined) => {
+export const operateAuthValueToDisabled = (operateAuthValue?: number) => {
   // 值为0 无权限 返回true
   // 值为空 或 1 有权限 则返回false
   if (operateAuthValue === 0) return true
@@ -10,21 +10,21 @@ export const operateAuthValueToDisabled = (operateAuthValue: number | undefined)
 }
 
 export const useAuth = () => {
-  const { pathname } = useLocation()
+  const matches = useMatches()
+  const id = matches[matches.length - 1].id
   const menuData = useAppSelector((state) => state.menuData.data)
   // 操作权限
   let operateAuth: OperateAuthType = {}
-  const findOperateAuth = (list: MenuDataItemType[], parentPath?: string) => {
-    for (let i = 0; i < list.length; i++) {
-      const record = list[i]
-      if (`${parentPath ?? ''}${record.path}` === pathname) {
-        if (record.operateAuth) operateAuth = record.operateAuth
-        break
-      } else if (record.children && record.children.length)
-        findOperateAuth(record.children, `${parentPath ?? ''}${record.path}`)
+  let iterativeMenuData = [...menuData]
+  while (iterativeMenuData.length) {
+    const record = iterativeMenuData.shift() as MenuDataItemType
+    if (record.pageUrl && record.pageUrl.split('/')[1] === id) {
+      if (record.operateAuth) operateAuth = record.operateAuth
+      break
+    } else if (record.children && record.children.length) {
+      iterativeMenuData = [...record.children, ...record.children]
     }
   }
-  findOperateAuth(menuData)
   // TODO 其他权限...
   return {
     operateAuth
