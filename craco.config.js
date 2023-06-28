ac06f4801fb64d4d64e12b3cdb1cf8546239d969
leiboT <superviho1@gmail.com>
2023-06-28 18:36:12 +0800

    ci: 打包分析&splitChunks

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,15 @@
 const path = require('path')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+let plugins = []
+if (process.env.NODE_ENV === 'production') plugins.push(new BundleAnalyzerPlugin())
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src')
-    }
+    },
+    plugins
   },
   devServer: {
     client: {
@@ -14,5 +19,40 @@ module.exports = {
         runtimeErrors: false
       }
     }
-  }
+  },
+  plugins: [
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          if (process.env.NODE_ENV === 'production')
+            webpackConfig.optimization.splitChunks = {
+              // all
+              chunks: 'async',
+              minSize: 20000,
+              minRemainingSize: 0,
+              minChunks: 1,
+              maxAsyncRequests: 30,
+              maxInitialRequests: 30,
+              enforceSizeThreshold: 50000,
+              cacheGroups: {
+                defaultVendors: {
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: -10,
+                  reuseExistingChunk: true
+                },
+                default: {
+                  minChunks: 2,
+                  priority: -20,
+                  reuseExistingChunk: true
+                }
+                // mockjs: {
+                //   test: /[\\/]node_modules[\\/]mockjs[\\/]/
+                // }
+              }
+            }
+          return webpackConfig
+        }
+      }
+    }
+  ]
 }
